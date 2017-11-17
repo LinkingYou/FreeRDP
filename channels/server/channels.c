e76a9fc8cad11309ab0201750c5b7f962c67148c
Armin Novak <armin.novak@thincast.com>
2017-11-17 11:24:42 +0100

    Fixed disabling of channels.

--- channels/server/channels.c
+++ channels/server/channels.c
@@ -39,49 +39,79 @@
  * this is a workaround to force importing symbols
  * will need to fix that later on cleanly
  */
-
+#if defined(CHANNEL_AUDIN_SERVER)
 #include <freerdp/server/audin.h>
+#endif
+#if defined(CHANNEL_RDPSND_SERVER)
 #include <freerdp/server/rdpsnd.h>
+#endif
+#if defined(CHANNEL_CLIPRDR_SERVER)
 #include <freerdp/server/cliprdr.h>
+#endif
+#if defined(CHANNEL_ECHO_SERVER)
 #include <freerdp/server/echo.h>
+#endif
+#if defined(CHANNEL_RDPDR_SERVER)
 #include <freerdp/server/rdpdr.h>
+#endif
+#if defined(CHANNEL_RDPEI_SERVER)
 #include <freerdp/server/rdpei.h>
+#endif
+#if defined(CHANNEL_DRDYNVC_SERVER)
 #include <freerdp/server/drdynvc.h>
+#endif
+#if defined(CHANNEL_REMDESK_SERVER)
 #include <freerdp/server/remdesk.h>
+#endif
+#if defined(CHANNEL_ENCOMSP_SERVER)
 #include <freerdp/server/encomsp.h>
+#endif
+#if defined(CHANNEL_RDPGFX_SERVER)
 #include <freerdp/server/rdpgfx.h>
+#endif
 
-void freerdp_channels_dummy() 
+void freerdp_channels_dummy(void)
 {
+#if defined(CHANNEL_AUDIN_SERVER)
 	audin_server_context_new(NULL);
 	audin_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_RDPSND_SERVER)
 	rdpsnd_server_context_new(NULL);
 	rdpsnd_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_CLIPRDR_SERVER)
 	cliprdr_server_context_new(NULL);
 	cliprdr_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_ECHO_SERVER)
 	echo_server_context_new(NULL);
 	echo_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_RDPDR_SERVER)
 	rdpdr_server_context_new(NULL);
 	rdpdr_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_DRDYNVC_SERVER)
 	drdynvc_server_context_new(NULL);
 	drdynvc_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_RDPEI_SERVER)
 	rdpei_server_context_new(NULL);
 	rdpei_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_REMDESK_SERVER)
 	remdesk_server_context_new(NULL);
 	remdesk_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_ENCOMSP_SERVER)
 	encomsp_server_context_new(NULL);
 	encomsp_server_context_free(NULL);
-
+#endif
+#if defined(CHANNEL_RDPGFX_SERVER)
 	rdpgfx_server_context_new(NULL);
 	rdpgfx_server_context_free(NULL);
+#endif
 }
 
 /**
